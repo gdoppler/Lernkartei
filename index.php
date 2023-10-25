6f5e61a7eab7e64e92cb2918c69f67e28e27f1b8
gdoppler <gdoppler2020@gmail.com>
2023-10-25 12:33:02 +0100

    for loop for questions

--- index.php
+++ index.php
@@ -49,67 +49,32 @@
 
 
 <form action="index.php" method="post">
-    <?php $questionNumber = rand(0,count($questionsAnswers)) ?>
-    <p><?= $questionsAnswers[$questionNumber][0] ?></p>
+    <?php
+    for($x=1; $x<=3; $x++){ 
+    ?>
+        <?php $questionNumber = rand(0,count($questionsAnswers)) ?>
+        <p><?= $questionsAnswers[$questionNumber][0] ?></p>
 
-    <div class="<?=$classGreen?>">
-        <label>
-            <input type="radio" name="answer" value="Korrekt"   >
-        </label>
-        <?= $questionsAnswers[$questionNumber][1] ?>
-    </div>
-    <div class="<?=$classRed?>">
-        <label>
-            <input type="radio" name="answer" value="Falsch"    >
-        </label>
-        <?= $questionsAnswers[$questionNumber][2] ?> <br>
-        <label>
-            <input type="radio" name="answer" value="Falsch"     >
-        </label>
-        <?= $questionsAnswers[$questionNumber][3] ?> <br>
-    </div>
-    <br>
-    <?php $questionNumber = rand(0,count($questionsAnswers)) ?>
-    <p><?= $questionsAnswers[$questionNumber][0] ?></p>
+        <div class="<?=$classGreen?>">
+            <label>
+                <input type="radio" name="answer<?=$x?>" value="Korrekt"   >
+            </label>
+            <?= $questionsAnswers[$questionNumber][1] ?>
+        </div>
+        <div class="<?=$classRed?>">
+            <label>
+                <input type="radio" name="answer<?=$x?>" value="Falsch"    >
+            </label>
+            <?= $questionsAnswers[$questionNumber][2] ?> <br>
+            <label>
+                <input type="radio" name="answer<?=$x?>" value="Falsch"     >
+            </label>
+            <?= $questionsAnswers[$questionNumber][3] ?> <br>
+        </div>
+        <br>
+    <?php }?>
 
-    <div class="<?=$classGreen?>">
-        <label>
-            <input type="radio" name="answer" value="Korrekt"   >
-        </label>
-        <?= $questionsAnswers[$questionNumber][1] ?>
-    </div>
-    <div class="<?=$classRed?>">
-        <label>
-            <input type="radio" name="answer" value="Falsch"    >
-        </label>
-        <?= $questionsAnswers[$questionNumber][2] ?> <br>
-        <label>
-            <input type="radio" name="answer" value="Falsch"     >
-        </label>
-        <?= $questionsAnswers[$questionNumber][3] ?> <br>
-    </div>
-    <br>
-    <?php $questionNumber = rand(0,count($questionsAnswers)) ?>
-    <p><?= $questionsAnswers[$questionNumber][0] ?></p>
-
-    <div class="<?=$classGreen?>">
-        <label>
-            <input type="radio" name="answer" value="Korrekt"   >
-        </label>
-        <?= $questionsAnswers[$questionNumber][1] ?>
-    </div>
-    <div class="<?=$classRed?>">
-        <label>
-            <input type="radio" name="answer" value="Falsch"    >
-        </label>
-        <?= $questionsAnswers[$questionNumber][2] ?> <br>
-        <label>
-            <input type="radio" name="answer" value="Falsch"     >
-        </label>
-        <?= $questionsAnswers[$questionNumber][3] ?> <br>
-    </div>
-    <br>
-
+    
 
 
 
